ff5f9362f280d5d144f9bd0a0918d53b57fc53ba
ashishOjha2005 <133503533+ashishOjha2005@users.noreply.github.com>
2026-09-26 23:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.54%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -1,16 +1,24 @@
  class Solution { 
     public int countDigits(int num) { 
         int val = 0; 
-        int originalNum = num;  
         
-        while (num > 0) {
-            int digit = num % 10;  
+         
+        int temp = num;
+        while (temp > 0) {
+            int d = temp % 10;  
             
-            if (digit != 0 && originalNum % digit == 0) { 
-                val++;  
-            }
+             
+            if (d == 1 && num % 1 == 0) { val++; }
+            else if (d == 2 && num % 2 == 0) { val++; }
+            else if (d == 3 && num % 3 == 0) { val++; }
+            else if (d == 4 && num % 4 == 0) { val++; }
+            else if (d == 5 && num % 5 == 0) { val++; }
+            else if (d == 6 && num % 6 == 0) { val++; }
+            else if (d == 7 && num % 7 == 0) { val++; }
+            else if (d == 8 && num % 8 == 0) { val++; }
+            else if (d == 9 && num % 9 == 0) { val++; }
             
-            num /= 10;  
+            temp = temp / 10;  
         }
         
         return val; 
